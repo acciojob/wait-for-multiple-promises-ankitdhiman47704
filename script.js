3052011003472ae97b1ba40824a595112f571624
Ankit Dhiman <63338248+ankitdhiman47704@users.noreply.github.com>
2023-03-24 16:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-//your JS code here. If required.
 let promise1Time = 0;
 let promise2Time = 0;
 let promise3Time = 0;
-
+console.log(new Date());
 const promise1 = new Promise((resolve,reject)=>{
     let prom1randomTime = Math.floor(Math.random()*3+1);
     promise1Time = prom1randomTime;
     setTimeout(() => {
+		console.log(new Date());
+
         resolve("prom 1 resolved");
-        
+
     }, prom1randomTime*1000);
 });
 const promise2 = new Promise((resolve,reject)=>{
     let prom2RandomTime = Math.floor(Math.random()*3+1);
     promise2Time = prom2RandomTime;
     setTimeout(() => {
+		console.log(new Date());
+
         resolve("prom 2 resolved");
         
     }, prom2RandomTime*1000);
@@ -23,15 +26,19 @@ const promise3 = new Promise((resolve,reject)=>{
     let prom3RandomTime = Math.floor(Math.random()*3+1);
     promise3Time = prom3RandomTime;
     setTimeout(() => {
+		console.log(new Date());
+
         resolve("prom 3 resolved");
         
     }, prom3RandomTime*1000);
 });
 
 
-
+  const startingTime = new Date();
  Promise.all([promise1,promise2,promise3]).then(()=>{
-    const tablehead = document.getElementById("thead");
+    
+  const finalTime =  new Date();
+const tablehead = document.getElementById("thead");
     const loadRow = document.querySelector(".loadingRow");
     tablehead.removeChild(loadRow);
 	 const body = document.getElementById("output");
@@ -61,7 +68,6 @@ const promise3 = new Promise((resolve,reject)=>{
     let row4c1 = fourthRow.insertCell(0);
     row4c1.innerText = "Total";
     let row4c2 = fourthRow.insertCell(1);
-    row4c2.innerText = (promise1Time+promise2Time+promise3Time).toFixed(3);
+    row4c2.innerText = (finalTime-startingTime)/1000;
     body.appendChild(fourthRow);
-
- })
+ });
